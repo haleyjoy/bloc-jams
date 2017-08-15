6ad3fcb84060599d4adf5337069e16e796757da0
Haley Guilfoile <koala.la.love@gmail.com>
2017-08-15 16:05:25 -0500

    adjusted landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,24 @@
- var pointsArray = document.getElementsByClassName('point');
- 
-    
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translate(0)";        point.style.msTransform = "scaleX(1) translate(0)";      point.style.WebkitTransform = "scaleX(1) translate(0)";
-};
-    
- var animatePoints = function() {
-     var revealPoint = function() {
-         $(this).css({
-             opacity: 1,
-             transform: 'scaleX(1) translateY(0)'
-         });
+var animatePoints = function() {
+    var revealPoint = function() {
+  $(this).css({
+  opacity: 1,
+  transform: 'scaleX(1) translateY(0)'
+  });
+  };
     $.each($('.point'), revealPoint);
+ };
 
-};
-};
 
- $(window).load(function() {
+$(window).load(function() {
+  if ($(window).height() > 950) {
+  animatePoints();
+  } 
 
-    if ($(window).height() > 950) {
-         animatePoints();
-     }
-    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    
-     $(window).scroll(function(event) {
-         if ($(window).scrollTop() >= scrollDistance) {
-             animatePoints();   
-         }
-     });
- });
+  $(window).scroll(function(event) {
+  if ($(window).scrollTop() >= scrollDistance) {
+  animatePoints();
+  }
+  });
+});
